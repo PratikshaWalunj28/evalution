4ba76df04daafa2ca30f7b5b27404d652624858f
pratiksha <pratikshawalunj28@gmail.com>
2025-11-03 22:32:53 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 df=spark.read.csv("data.csv", header=True, inferSchema=True)
 df.show()   
+=======
+df1=spark.read.csv("file1.csv", header=True, inferSchema=True)
+df.show()
+>>>>>>> Stashed changes
